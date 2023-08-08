fef464fe3b27df934e2bc7ef29962d834f7abdbe
mktcode <kontakt@markus-kottlaender.de>
2023-08-08 18:11:15 +0200

    something to start with

---
+++ lua/mktcode/init.lua
@@ -0,0 +1,14 @@
+require("mktcode.remap")
+require("mktcode.packer")
+
+vim.o.number = true
+vim.o.autoindent = true
+vim.o.smartindent = true
+vim.o.mouse = 'a'
+vim.o.cursorline = true
+vim.cmd([[highlight CursorLine ctermbg=0 cterm=NONE guibg=lightgrey]])
+vim.cmd([[highlight CursorLineNr cterm=NONE gui=NONE]])
+vim.o.expandtab = true
+vim.o.hlsearch = true
+vim.o.shiftwidth = 2
+vim.o.syntax = 'on'
---
+++ lua/mktcode/packer.lua
@@ -0,0 +1,12 @@
+-- This file can be loaded by calling `lua require('plugins')` from your init.vim
+
+-- Only required if you have packer configured as `opt`
+vim.cmd [[packadd packer.nvim]]
+
+return require('packer').startup(function(use)
+  -- Packer can manage itself
+  use 'wbthomason/packer.nvim'
+  use 'github/copilot.vim'
+  use { 'nvim-telescope/telescope.nvim', tag = '0.1.2', requires = { {'nvim-lua/plenary.nvim'} }
+}
+end)
