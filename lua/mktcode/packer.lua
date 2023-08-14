39129ab30d701bc5944a87011d55697a913affe3
mktcode <kontakt@markus-kottlaender.de>
2023-08-14 12:31:31 +0200

    update config

--- lua/mktcode/packer.lua
+++ lua/mktcode/packer.lua
@@ -6,7 +6,32 @@ vim.cmd [[packadd packer.nvim]]
 return require('packer').startup(function(use)
   -- Packer can manage itself
   use 'wbthomason/packer.nvim'
+
   use 'github/copilot.vim'
-  use { 'nvim-telescope/telescope.nvim', tag = '0.1.2', requires = { {'nvim-lua/plenary.nvim'} }
-}
+
+  use {
+    'nvim-telescope/telescope.nvim',
+    tag = '0.1.2',
+    requires = {
+      {'nvim-lua/plenary.nvim'}
+    }
+  }
+
+  use {
+    'nvim-treesitter/nvim-treesitter',
+    run = function()
+    local ts_update = require('nvim-treesitter.install').update({ with_sync = true })
+      ts_update()
+    end,
+  }
+
+  use({
+    'rose-pine/neovim',
+    as = 'rose-pine',
+    config = function()
+      vim.cmd('colorscheme rose-pine-dawn')
+    end
+  })
+
+  use('theprimeagen/harpoon')
 end)
