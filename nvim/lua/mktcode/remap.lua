3376b5e7797ab1af98d3fe16df634c7c1be1a123
mktcode <kontakt@markus-kottlaender.de>
2023-08-31 14:02:48 +0200

    added vim tab mappings

--- nvim/lua/mktcode/remap.lua
+++ nvim/lua/mktcode/remap.lua
@@ -1,2 +1,11 @@
 vim.g.mapleader = " "
 vim.api.nvim_set_keymap('n', '<leader>pv', ':Ex<CR>', {noremap = true, silent = true})
+
+-- tabs
+vim.api.nvim_set_keymap('n', '<leader>tc', ':tabnew<CR>', {noremap = true, silent = true})
+vim.api.nvim_set_keymap('n', '<leader>tp', ':tabprev<CR>', {noremap = true, silent = true})
+vim.api.nvim_set_keymap('n', '<leader>tn', ':tabnext<CR>', {noremap = true, silent = true})
+vim.api.nvim_set_keymap('n', '<leader>th', ':tabmove -1<CR>', {noremap = true, silent = true})
+vim.api.nvim_set_keymap('n', '<leader>tl', ':tabmove +1<CR>', {noremap = true, silent = true})
+vim.api.nvim_set_keymap('n', '<leader>tq', ':tabclose<CR>', {noremap = true, silent = true})
+
