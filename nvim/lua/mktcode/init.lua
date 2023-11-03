b1c7b0abae3422a57320abf0e5df2f44036f1bd7
mktcode <kontakt@markus-kottlaender.de>
2023-11-03 12:53:59 +0100

    adjustments

--- nvim/lua/mktcode/init.lua
+++ nvim/lua/mktcode/init.lua
@@ -3,7 +3,7 @@ require("mktcode.packer")
 
 vim.o.number = true
 vim.o.relativenumber = true
-vim.o.scrolloff = 8
+vim.o.scrolloff = 5
 vim.o.autoindent = true
 vim.o.smartindent = true
 vim.o.mouse = 'a'
@@ -12,5 +12,7 @@ vim.cmd([[highlight CursorLine ctermbg=0 cterm=NONE guibg=lightgrey]])
 vim.cmd([[highlight CursorLineNr cterm=NONE gui=NONE]])
 vim.o.expandtab = true
 vim.o.hlsearch = true
-vim.o.shiftwidth = 4
+vim.o.shiftwidth = 2
 vim.o.syntax = 'on'
+
+vim.g.copilot_filetypes = {markdown = true, yaml = true}
